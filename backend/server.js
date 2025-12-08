1de7bdb0e7a8caf6fc64d35372000aea2f957370
Himanshu meena <himanshu83065742@gmail.com>
2025-12-09 00:36:54 +0530

    Update in server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/Product.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
@@ -9,13 +9,23 @@ const app = express();
 
 app.use(express.json()); //allow us to accept json data in req.body
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.error("Error in fetching products", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   const product = req.body; //user will send this data
 
   if (!product.name || !product.price || !product.image) {
     return res
       .status(400)
-      .json({ success: false, message: "All fields are required" });
+      .json({ success: false, message: "Please fill all the fields" });
   }
 
   const newProduct = new Product(product);
@@ -29,9 +39,43 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product ID" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    console.error("Error in deleting product", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
 console.log(process.env.MONGO_URI);
 
-app.listen(50, () => {
+app.listen(5001, () => {
   connectDB();
-  console.log("Server is started at http://localhost:50");
+  console.log("Server is started at http://localhost:5001");
 });
+0;
