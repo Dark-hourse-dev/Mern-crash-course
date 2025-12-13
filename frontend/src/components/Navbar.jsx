28014bf7c6c9f7a706b20b7256c12a92677f3e90
Himanshu meena <himanshu83065742@gmail.com>
2025-12-13 15:08:42 +0530

    Modification

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import {
+  Button,
+  HStack,
+  Container,
+  Flex,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { PlusSquareIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
-  return <div></div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip={"text"}
+        >
+          <Link to="/">Product store </Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <PlusSquareIcon fontSize={20} />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? "🌙" : "🔆"} Mode
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
 };
 
 export default Navbar;
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CreatePage = () => {
-  return <div></div>;
+  return <div>Create page</div>;
 };
 
 export default CreatePage;
